d475cc25324f66785eeca75668a74d84f68e1da2
pravin shetty <pravins2@illinois.edu>
2024-07-13 15:50:39 -0600

    Initial checkin

---
+++ start-project/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import reactImg from '../../assets/react-core-concepts.png';
+import './Header.css';
+const reactDescriptions = ['Fundamental','Crucial','Core'];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export default function Header() {
+  const description = reactDescriptions[getRandomInt(2)];
+  return (<header>
+    <img src={reactImg} alt="Stylized atom" />
+    <h1>Pravin's React Project</h1>
+    <p>
+      {description} React concepts you will need for almost any app you are
+      going to build!
+    </p>
+  </header>
+  );
+}
---
+++ start-project/src/components/TabButton.jsx
@@ -0,0 +1,6 @@
+export default function TabButton({children, onSelect, isSelected}) {
+  function handleClick() {
+    console.log("Hello world");
+  }
+  return (<li><button className={isSelected ? 'active': undefined} onClick={onSelect}>{children}</button></li>);
+}
